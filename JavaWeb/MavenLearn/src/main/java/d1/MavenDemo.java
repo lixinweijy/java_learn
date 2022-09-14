517415ca9fba967d7a3a805ab665fa8d299bbe7b
lxw <2030277353@qq.com>
2022-09-14 23:05:25 +0800

    maven

---
+++ JavaWeb/MavenLearn/src/main/java/d1/MavenDemo.java
@@ -0,0 +1,7 @@
+package d1;
+
+public class MavenDemo {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
---
+++ JavaWeb/maven-project/maven常用命令.md
@@ -0,0 +1,10 @@
+### Maven常用命令
+编译 mvn compile
+
+清除 mvn clean
+
+打包 mvn package
+
+测试 mvn test
+
+安装 mvn install
